56120f5ebe1eb389e5914050fcf376b42e1ce753
Danzel Ferdinand <danzelferdinand@gmail.com>
2026-06-11 19:44:06 +0700

    Fixed save issue

--- Assets/Scripts/SavingLoading.cs
+++ Assets/Scripts/SavingLoading.cs
@@ -1,32 +1,40 @@
 using UnityEngine;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
 using Unity.VisualScripting;
 
 public static class SavingLoading
 {
     public static void savePlayer(Player player)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.data";
-        FileStream stream = new FileStream(path, FileMode.Create);
+        string path = Application.persistentDataPath + "player.save";
+        // BinaryFormatter formatter = new BinaryFormatter();
+        // FileStream stream = new FileStream(path, FileMode.Create);
         PlayerData data = new PlayerData(player);
 
-        formatter.Serialize(stream, data);
-        stream.Close();
+        // New Save function
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(path, json);
+
+
+        // formatter.Serialize(stream, data);
+        // stream.Close();
     }
 
     public static PlayerData loadPlayer()
     {
-        string path = Application.persistentDataPath + "/player.data";
+        string path = Application.persistentDataPath + "player.save";
         if (File.Exists(path))
         {
             
-            BinaryFormatter formatter = new BinaryFormatter();
+            // BinaryFormatter formatter = new BinaryFormatter();
             
-            FileStream stream = new FileStream(path, FileMode.Open);
+            // FileStream stream = new FileStream(path, FileMode.Open);
 
-            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            // PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+            //Newsave
+            string json = File.ReadAllText(path);
+            PlayerData data = JsonUtility.FromJson<PlayerData>(json);
 
             return data;
         }
